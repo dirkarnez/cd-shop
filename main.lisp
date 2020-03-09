b9df13df409da33e86ee1a2d1e14cd786d4c4611
dirkarnez <smalldirkalex@gmail.com>
2020-03-10 01:58:23 +0800

    - init

---
+++ main.lisp
@@ -0,0 +1,14 @@
+(defvar *db* nil)
+
+(defun create-cd (title artist)
+  (list :title title :artist artist))
+
+(defun add-cd (cd)
+  (push cd *db*))
+
+(add-cd (create-cd "Inception OST" "Hans Zimmer"))
+
+(defun dump-db ()
+  (dolist (cd *db*)
+    (format t "~{~a:~10t~a~%~}~%" cd)))
+
